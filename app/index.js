0eaa5c792f057f99c12d930f196c84e6091637f6
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-04-24 14:21:58 -0300

    feat: create project

---
+++ app/index.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text } from "react-native";
+
+import axios from "axios";
+
+import FilmCard from "../components/FilmCard.js";
+import SkeletonCard from "../components/SkeletonCard.js";
+
+export default function Home() {
+  // Cria um estado chamado Films para guardar a lista de filmes
+  const [films, setFilms] = useState([]);
+
+  // Cria um estado chamado loading para saber se os filmes estão sendo carregados
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get("https://ghibliapi.vercel.app/films")
+        .then((res) => setFilms(res.data))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+    }, 5000);
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.header}>🎥 Filmes Studio Ghibli</Text>
+
+        {
+            loading ? (
+                <FlatList data={[1,2,3,4,5]}
+                keyExtractor={(item) => item.toString()}
+                renderItem={() => <SkeletonCard/>}
+                contentContainerStyle={styles.list}/>
+            ) : (
+                <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => <FilmCard film={item} />}
+                    contentContainerStyle={styles.list}
+                />
+            )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#191414",
+    },
+    header: {
+        fontSize: 28,
+        fontWeight: "bold",
+        color: "#fff",
+        textAlign: "center",
+        marginVertical: 16
+    },
+    list: {
+        paddingHorizontal: 16,
+        paddingBottom: 20,
+    },
+})
